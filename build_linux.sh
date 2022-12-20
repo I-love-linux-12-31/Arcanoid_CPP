7f6e10b132930b1e0a6341a12659514b58ba1fbd
yaroslav_admin <I-love-linux-12-31@users.noreply.github.com>
2022-12-20 22:55:14 +0300

    Build improvements

--- build_linux.sh
+++ build_linux.sh
@@ -5,12 +5,18 @@ cmake -S ./ -B ./Build
 # cp ./GUI ./Build -r
 cp ./icons ./Build -r
 cp ./levels ./Build -r
+mkdir ./Build/bin
 cd ./Build
 make
+mv ./Arcanoid_cpp ./bin/
+
 rm ./.qt -r
 rm ./CMakeFiles -r
 rm ./cmake_install.cmake
 rm ./CMakeCache.txt
-rm ./Makefile
+rm ./Arcanoid_cpp_autogen -r
+rm Makefile
+
+
 
 
